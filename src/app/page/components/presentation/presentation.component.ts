96ce275d4a68d9262d9d95878b77fcda1eb8149f
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-03 22:49:23 -0300

    feat: adiciona componente presentation

--- src/app/page/components/presentation/presentation.component.ts
+++ src/app/page/components/presentation/presentation.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss']
 })
-export class PresentationComponent {
+export class PresentationComponent implements OnInit{
+	idade : number = 0;
 
+	ngOnInit(): void {
+		this.idade = this.calcAge("Mon Dec 02 2002 12:00:00 GMT-0300 (BRT)");
+	}
+
+	calcAge(dateString: string) {
+		var birthday = +new Date(dateString);
+		return ~~((Date.now() - birthday) / (31557600000));
+	  }	
 }

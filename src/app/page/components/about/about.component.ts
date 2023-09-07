01df37cda01721b5fc00e5f315c463152390c238
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-09-06 23:09:04 -0300

    fix: add component about

--- src/app/page/components/about/about.component.ts
+++ src/app/page/components/about/about.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+	idade : number = 0;
 
+	ngOnInit(): void {
+		this.idade = this.calcAge("Mon Dec 02 2002 12:00:00 GMT-0300 (BRT)");
+	}
+
+	calcAge(dateString: string) {
+		var birthday = +new Date(dateString);
+		return ~~((Date.now() - birthday) / (31557600000));
+	}
 }

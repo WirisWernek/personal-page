a3beaf07b5d28459105a97cc5e61f0b7dcc5bd70
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-11-04 22:39:31 -0300

    refactor: move componentes do módulo para o modulo principal, adiciona roteamento entre os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { CommandBarComponent } from './components/command-bar/command-bar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { ProjectLeftComponent } from './components/project-left/project-left.component';
+import { ProjectRightComponent } from './components/project-right/project-right.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SendEmailComponent } from './components/send-email/send-email.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	NavbarComponent,
+    PresentationComponent,
+    AboutComponent,
+    TechnologiesComponent,
+    ProjectsComponent,
+    ProjectLeftComponent,
+    ProjectRightComponent,
+    ExperiencesComponent,
+    SendEmailComponent,
+    SocialMediaComponent,
+    ContactComponent,
+    CommandBarComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
